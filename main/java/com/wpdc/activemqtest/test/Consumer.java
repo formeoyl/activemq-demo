10e343342a21500676fecf22f4ab8fea87655b28
oyl <846764485@qq.com>
2018-07-20 15:25:55 +0800

    springboot整合activemq的demo

---
+++ main/java/com/wpdc/activemqtest/test/Consumer.java
@@ -0,0 +1,31 @@
+package com.wpdc.activemqtest.test;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Consumer {
+	
+	@JmsListener(destination = "queue-1")
+	public void receiveQueue(String text) {
+		System.out.println("1-queue-消费："+text);
+	}
+
+	@JmsListener(destination = "queue-1")
+	public void receiveQueue2(String text) {
+		System.out.println("2-queue-消费："+text);
+	}
+
+	@JmsListener(destination = "topic-1", containerFactory="topicListenerContainer")
+	public void receiveTopic(String text) {
+		System.out.println("1-topic-消费"+text);
+
+	}
+
+	@JmsListener(destination = "topic-1", containerFactory="topicListenerContainer")
+	public void receiveTopic2(String text) {
+		System.out.println("2-topic-消费"+text);
+
+	}
+}
---
+++ main/java/com/wpdc/activemqtest/test/Producer.java
@@ -0,0 +1,38 @@
+package com.wpdc.activemqtest.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.jms.Destination;
+import javax.jms.Queue;
+import javax.jms.Topic;
+
+@Controller
+public class Producer {
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+//    @Autowired
+//    private Queue queue;
+//    @Autowired
+//    private Topic topic;
+
+    @Resource(name = "queue1")
+    private Destination queue;
+
+    @Resource(name = "topic1")
+    private Destination topic;
+
+    @ResponseBody
+    @RequestMapping("/sendMsg")
+    public void send(String msg) {
+        for(int i=0;i<10;i++){
+            this .jmsMessagingTemplate.convertAndSend(this.queue, "queue"+i);
+            this.jmsMessagingTemplate.convertAndSend(this.topic, "topic"+i);
+        }
+    }
+}
